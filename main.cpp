e339ba216c4420fda3219bd59b7fc4dcc5af927c
the-timoye <thetimoye@gmail.com>
2025-04-01 14:58:05 +0100

    feat: read SA in binary format

--- main.cpp
+++ main.cpp
@@ -15,28 +15,36 @@ optional<vector<int>> createHierarchy(const string& S, const string& Q, int l,
     int level = 0);
 
 vector<int> readSuffixArrayBinary(const string& filename) {
-    vector<int> SA;
     ifstream file(filename, ios::binary);
 
-    if (!file) {
-        cerr << "Error opening file: " << filename << endl;
+    if (!file){
+        std::cerr << "Error opening file: " << filename << endl;
         exit(1);
     }
 
-    int32_t index;  // 4-byte integer
-    while (file.read(reinterpret_cast<char*>(&index), sizeof(int32_t))) {
-        SA.push_back(index);
-    }
+    file.seekg(0, ios::end);
+    size_t fileSize = file.tellg();
+    file.seekg(0, ios::beg);
+
+    if (fileSize % sizeof(int) != 0) {
+        cerr << "Invalid file format! " << endl;
+        exit(1);
+    };
+
+    size_t numElements = fileSize/sizeof(int);
+    vector<int> SA(numElements);
+
+    file.read(reinterpret_cast<char*>(SA.data()), fileSize);
 
     file.close();
     return SA;
 }
 
 int main() {
-    string S = "ATGCCTGATGC";
-    string Q = "CCTA";
+    string S = "AATTCCCCCGACTGATTTAACCGGTTAAAGCCTTACC";
+    string Q = "TTAA";
     // vector<int> SA = {11, 7, 0, 10, 3, 4, 6, 9, 2, 5, 8, 1};
-    int l = 2;  // Pruning parameter
+    int l = 3;  // Pruning parameter
 
     vector<int> SA = readSuffixArrayBinary("output");
 
@@ -50,7 +58,7 @@ int main() {
 
     optional<vector<int>> result = createHierarchy(S, Q, l, SA);
 	finish = clock();
-	duration = (double)(finish - start) / 1000;
+	duration = (double)(finish - start) / CLOCKS_PER_SEC;
 
 	std::cout << "Time: " << duration << std::endl;
 
