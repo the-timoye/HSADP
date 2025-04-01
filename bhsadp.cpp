70e7492f0e7963194e5bbdae57dba41281b6790d
the-timoye <thetimoye@gmail.com>
2025-04-01 14:58:58 +0100

    feat: handle hsadp as binary

---
+++ bhsadp.cpp
@@ -0,0 +1,147 @@
+/*
+* An implementation of HSADP handling binary strings and binary SAs
+* Optimizations:
+    - hashes of size_t instead of strings. That is, 8bytes >> 32bytes
+*/
+
+#include <iostream>
+#include <fstream>
+#include <unordered_map>
+#include <optional>
+#include <vector>
+#include <cstring> // used by memcpy
+
+#define __LOG std::cout
+#define __LOGERR std::cerr
+#define __ENDLOG std::endl
+#define vector std::vector
+#define pair std::pair
+#define string std::string
+#define ios std::ios
+#define ifstream std::ifstream
+#define optional std::optional
+#define nullopt std::nullopt
+#define unordered_map std::unordered_map
+#define min std::min
+
+#define uchar unsigned char
+
+// reads the string as binary
+uchar* readS(const string& filename, size_t& size) {
+    ifstream file(filename, ios::binary | ios::ate);
+    if (!file) {
+        __LOGERR << "Error opening file" << filename << __ENDLOG;
+        exit(1);
+    };
+
+    size = file.tellg(); // find where in the stream the cursor is at
+    uchar* buffer = new uchar[size];
+    file.seekg(0, ios::beg);
+    file.read(reinterpret_cast<char*>(buffer), size);
+    file.close();
+    return buffer;
+};
+
+u_int32_t* readSA(const string& filename, size_t& size) {
+    ifstream file(filename, ios::binary | ios::ate);
+    if (!file) {
+        __LOGERR << "Error opening file" << filename << __ENDLOG;
+        exit(1);
+    };
+
+    size = file.tellg() / sizeof(u_int32_t);
+    u_int32_t* buffer = new u_int32_t[size];
+    file.seekg(0, ios::beg);
+    file.read(reinterpret_cast<char*>(buffer), size * sizeof(u_int32_t));
+    file.close();
+    return buffer;
+};
+
+void printHierarchy(const unordered_map<size_t, vector<u_int32_t>>& hier, const uchar* S, int substring_length) {
+    __LOG << "Tree Structure: { ";
+    for (const auto& [hash, values] : hier) {
+        if (!values.empty()) {
+            string reconstructed_substring(reinterpret_cast<const char*>(S + values[0]), substring_length);
+            __LOG << "\"" << reconstructed_substring << "\": [ ";
+            for (u_int32_t v : values) __LOG << v << " ";
+            __LOG << "] ";
+        }
+    }
+    __LOG << "}" << __ENDLOG;
+}
+
+
+optional<vector<u_int32_t>> createHierarchy(
+    const uchar* S
+    , const uchar* Q
+    , int l
+    , const u_int32_t* SA
+    , size_t n
+    , size_t m
+    , size_t SA_size
+    , const optional<pair<const uchar*, vector<u_int32_t>>> valid_indicies = nullopt
+    , int level = 0
+) {
+    __LOG << "Level: " << level << __ENDLOG;
+
+    unordered_map<size_t, vector<u_int32_t>> hier; // TODO: find an alternative for size_t. it costs 8bytes!!
+    const u_int32_t* suffix_array = valid_indicies ? valid_indicies->second.data() : SA;
+    size_t SA_count = valid_indicies ? valid_indicies->second.size() : SA_size;
+    int new_l = (level > 0) ? ((m < (l+l)) ? l + (m-l) : (l+l)) : l;
+
+    for (size_t i=0; i < SA_count; i++) {
+        u_int32_t index = suffix_array[i];
+        if (index + new_l > n) continue;
+        size_t hash = 0;
+        memcpy(&hash, S+index, min(sizeof(size_t), (size_t)new_l));
+        hier[hash].push_back(index);
+    };
+
+    printHierarchy(hier, S, new_l);
+
+
+    optional<pair<const uchar*, vector<u_int32_t>>> nextValidIndicies = nullopt;
+    size_t qHash = 0;
+    memcpy(&qHash, Q, min(sizeof(size_t), (size_t)new_l));
+
+    auto it = hier.find(qHash);
+    if (it != hier.end()) {
+        __LOG << "String to COmpare: " << it->second[0] << " with: " << Q << __ENDLOG;
+        if (memcmp((S + it->second[0]), Q, m) == 0) return it->second;    
+        nextValidIndicies = {S+it->second[0], it->second};
+    };
+
+    if(!nextValidIndicies) {
+        __LOG << "No matching string found!" << __ENDLOG;
+        return nullopt;
+    }
+    return createHierarchy(S, Q, new_l, SA, n, m, SA_size, nextValidIndicies, level+1);
+};
+
+int main () {
+    size_t n, SA_size;
+    uchar* S = readS("input.txt", n);
+    u_int32_t* SA = readSA("output", SA_size);
+
+    uchar Q[] = "TTAA";
+    size_t m = sizeof(Q)-1;
+    int l = 3;
+
+    // Call function
+	clock_t start, finish;
+	double  duration;
+	start = clock();
+    optional<vector<u_int32_t>> result = createHierarchy(S, Q, l, SA, n, m, SA_size);
+	finish = clock();
+	duration = (double)(finish - start) / CLOCKS_PER_SEC;
+
+	std::cout << "Time: " << duration << std::endl;
+    if (result) {
+        for (u_int32_t index : *result) __LOG << index << __ENDLOG;
+    } else {__LOG << "No Exact Match Found." << __ENDLOG;};
+
+    delete[] S;
+    delete[] SA;
+
+    return 0;
+}
